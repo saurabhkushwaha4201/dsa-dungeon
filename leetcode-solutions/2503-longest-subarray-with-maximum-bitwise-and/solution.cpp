892958cc911a67d5a2c580f353e2d7c98f23368f
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2024-09-15 19:19:50 +0000

    [LeetCode Sync] Runtime - 105 ms (5.79%), Memory - 84.9 MB (100.00%)

---
+++ leetcode-solutions/2503-longest-subarray-with-maximum-bitwise-and/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) 
+    {
+        if (nums.empty()) {
+            return 0; // Return 0 if the vector is empty
+        }
+        int max_value= *max_element(nums.begin(),nums.end());
+        int len=1;
+        int temp_len=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==max_value)
+            {
+                temp_len++;
+                len=max(len,temp_len);
+            }
+            else
+            {
+                temp_len=0;
+            }
+            
+        }
+        return len;
+        
+    }
+};
