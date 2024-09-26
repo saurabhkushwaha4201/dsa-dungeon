4db4fc0038bf04e67982d14101dc00e3cd2da4dc
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2024-09-26 14:48:06 +0000

    [LeetCode Sync] Runtime - 3 ms (3.06%), Memory - 16.7 MB (30.85%)

--- leetcode-solutions/0061-rotate-list/solution.cpp
+++ leetcode-solutions/0061-rotate-list/solution.cpp
@@ -12,24 +12,28 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) 
     {
-        if(head == NULL||head->next == NULL||k == 0) return head;
-    //calculating length
-    ListNode* temp = head;
-    int length = 1;
-    while(temp->next != NULL) {
-        ++length;
-        temp = temp->next;
-    }
-    //link last node to first node
-    temp->next = head;
-    k = k%length; //when k is more than length of list
-    int end = length-k; //to get end of the list
-    while(end--) temp = temp->next;
-    //breaking last node link and pointing to NULL
-    head = temp->next;
-    temp->next = NULL;
+        if(head==NULL||head->next==NULL|| k==0)
+        return head;
+        ListNode* temp=head;
+        int length=1;
+        while(temp->next!=NULL)
+        {
+            length++;
+            temp=temp->next;
+        }
+        temp->next=head;
+        k=k%length;
+        int end=length-k;
         
-    return head;
+        while(end)
+        {
+            temp=temp->next;
+            end--;
+        }
+        head=temp->next;
+        temp->next=NULL;
+        return head;
+            
         
     }
 };
