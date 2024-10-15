396ef4de74fdc1eae5ea99b1ae7ce080935a18c8
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2024-10-15 03:49:38 +0000

    [LeetCode Sync] Runtime - 31 ms (0.78%), Memory - 30.2 MB (99.94%)

---
+++ leetcode-solutions/0792-binary-search/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int start=0;
+        int end = nums.size()-1;
+        int mid;
+        while(start <= end)
+        {
+            mid = start + ( end - start )/2;
+            if( nums[mid]== target)
+            {
+                return mid;
+            }
+            else if ( nums[mid] > target)   // mid k left side target hoga
+            {
+                end= mid - 1;
+            }
+            else            // target mid k right side hoga
+            {
+                start = mid+1;
+            }        
+            
+
+        }
+        return -1;
+        
+    }
+};
