1d4b9d310f05012efdd9490078a3d2622dc387ae
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2024-11-17 03:58:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 292 MB (99.11%)

---
+++ leetcode-solutions/3639-zero-array-transformation-i/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<int> diff(n + 1, 0);
+
+        for (const auto& query : queries) {
+            int l = query[0], r = query[1];
+            diff[l] +=1;
+            if (r + 1 < n) {
+                diff[r + 1] -= 1;
+            }
+        }
+
+        int currValue = 0;
+        for (int i = 0; i < n; ++i) {
+            currValue += diff[i]; 
+            if (currValue < nums[i]) {
+                return false; 
+            }
+        }
+
+        return true;
+    }
+};
