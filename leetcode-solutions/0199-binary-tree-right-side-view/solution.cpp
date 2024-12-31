2997f32645c5780452a7a4f6e3f36d31f3c9a77c
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2024-12-31 05:40:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15 MB (63.22%)

--- leetcode-solutions/0199-binary-tree-right-side-view/solution.cpp
+++ leetcode-solutions/0199-binary-tree-right-side-view/solution.cpp
@@ -9,33 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) 
     {
-        vector<int>ans;
-        if(root==NULL)
-        return ans;
-        queue<TreeNode*>q;
-        q.push(root);
+        vector<int> ans; // This will store the final right side view of the tree.
+        
+        if(root == NULL) // If the root is NULL (empty tree), return an empty vector.
+            return ans;
 
-        while(!q.empty())
+        queue<TreeNode*> q; // A queue is used for level-order traversal (BFS).
+        q.push(root); // Start with the root node.
+
+        while(!q.empty()) // Process all levels of the tree
         {
-            int size = q.size();
-            for(int i=0;i<size;i++)
+            int size = q.size(); // Get the number of nodes at the current level.
+            
+            // Loop through all the nodes in the current level
+            for(int i = 0; i < size; i++)
             {
-                TreeNode* curr = q.front();
-                q.pop();
-                if(i==size-1)
-                ans.push_back(curr->val);
-                if(curr->left)
-                q.push(curr->left);
-                if(curr->right)
-                q.push(curr->right);
+                TreeNode* curr = q.front(); // Get the front element of the queue (current node).
+                q.pop(); // Remove the front element from the queue.
 
+                // Check if we are at the last node of the current level (rightmost node).
+                if(i == size - 1)
+                    ans.push_back(curr->val); // Add the value of the rightmost node to the result.
+
+                // If the current node has a left child, add it to the queue for the next level.
+                if(curr->left)
+                    q.push(curr->left);
+                
+                // If the current node has a right child, add it to the queue for the next level.
+                if(curr->right)
+                    q.push(curr->right);
             }
         }
-        return ans;
-        
+        return ans; // Return the final vector containing the right side view of the tree.
     }
 };
+
