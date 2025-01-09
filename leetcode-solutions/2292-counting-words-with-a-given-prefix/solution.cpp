01cd592335c029d1402172bbd40ba21c2da597af
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-01-09 05:04:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.3 MB (45.28%)

---
+++ leetcode-solutions/2292-counting-words-with-a-given-prefix/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) 
+    {
+        int count = 0;
+        int len =pref.length();
+        for(int i=0;i<words.size();i++)
+        {
+            if(pref==words[i].substr(0,len))
+            {
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
