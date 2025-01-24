4956513df8d16815d351a5ccbc25d77ad3790fef
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-01-24 19:15:24 +0000

    [LeetCode Sync] Runtime - 4 ms (44.98%), Memory - 36.9 MB (95.95%)

---
+++ leetcode-solutions/0653-two-sum-iv---input-is-a-bst/solution.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>inorderValue;
+    void inorder(TreeNode* root)
+    {
+        if(root==NULL)
+        return;
+        inorder(root->left);
+
+        inorderValue.push_back(root->val);
+
+        inorder(root->right);
+    }
+    bool findTarget(TreeNode* root, int k) 
+    {
+        inorder(root);
+
+        int i =0;
+        int j = inorderValue.size()-1;
+
+        while(i<j)
+        {
+            int sum = inorderValue[i] + inorderValue[j];
+            if(sum==k)
+            return k;
+            else if (sum>k)
+            {
+                j--;
+            }
+            else 
+            i++;
+        }
+        return false;
+    }
+};
