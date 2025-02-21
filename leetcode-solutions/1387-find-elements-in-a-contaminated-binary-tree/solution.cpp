4e65753efd6775d78a31e18425dd479161212b69
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-02-21 05:52:34 +0000

    [LeetCode Sync] Runtime - 8 ms (68.38%), Memory - 34.7 MB (51.47%)

---
+++ leetcode-solutions/1387-find-elements-in-a-contaminated-binary-tree/solution.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    unordered_set<int>st;
+    void dfs(TreeNode*root,int data)
+    {
+        if(!root)
+        {
+            return;
+        }
+        root->val = data;
+        st.insert(data);
+        dfs(root->left,2*data+1);
+        dfs(root->right,2*data+2);
+    }
+    FindElements(TreeNode* root) {
+        dfs(root,0);
+    }
+    
+    bool find(int target) {
+        return st.count(target);
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
