46f840fbc8fd3f995dcf96a7ad95109ee537f13a
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-03-06 05:59:57 +0000

    [LeetCode Sync] Runtime - 20 ms (19.15%), Memory - 31.8 MB (9.44%)

---
+++ leetcode-solutions/3227-find-missing-and-repeated-values/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) 
+    {
+        unordered_map<int,int>mpp;
+        int n = grid.size();
+        for(int i =0;i<n;i++)
+        {
+            for(int j =0;j<n;j++)
+            {
+                mpp[grid[i][j]]++;
+            }
+        }
+        int a =-1;
+        int b = -1;
+        for(int i =1;i<=n*n;i++)
+        {
+            if(mpp[i]==0)
+            {
+                a = i;
+            }
+            else if(mpp[i]==2)
+            b = i;
+        }
+        return {b,a};
+
+        
+        
+    }
+};
