6803d3bf475d613da334284315ff07bb49de7e36
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-03-17 13:57:20 +0000

    [LeetCode Sync] Runtime - 5 ms (30.84%), Memory - 18.3 MB (24.01%)

---
+++ leetcode-solutions/2308-divide-array-into-equal-pairs/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) 
+    {
+        unordered_set<int>st;
+        for(int i =0;i<nums.size();i++)
+        {
+            if(st.count(nums[i]))
+            {
+                st.erase(nums[i]);
+            }
+            else
+            {
+                st.insert(nums[i]);
+            }
+        }
+        return st.empty();
+        
+    }
+};
