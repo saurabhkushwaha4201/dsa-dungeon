9a4da3c687201df32f43a9cc32122eff9b3aacb7
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-04-13 12:51:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (29.38%)

--- leetcode-solutions/0747-min-cost-climbing-stairs/solution.cpp
+++ leetcode-solutions/0747-min-cost-climbing-stairs/solution.cpp
@@ -1,48 +1,15 @@
 class Solution {
 public:
-
-    // int solve(vector<int>&cost,int index,vector<int>&dp)
-    // {
-    //     if(index>=cost.size())
-    //     {
-    //         return 0;
-    //     }
-    //     if(dp[index]!=-1)
-    //     return dp[index];
-    //     int first = cost[index] + solve(cost,index+1,dp);
-        
-    //     int second = cost[index]+ solve(cost,index+2,dp);
-    //     return dp[index]=min(first,second);
-    // }
     int minCostClimbingStairs(vector<int>& cost) {
-
         int n = cost.size();
-        // vector<int>dp(n+1);
+        vector<int>dp(n+1,0);
         
-        // dp[0] = 0;
-        // dp[1] = 0;
-        // for(int i = 2;i<=n;i++)
-        // {
-        //     int first = cost[i-1] + dp[i-1];
-        //     int second = cost[i-2]+dp[i-2];
-        //     dp[i] = min(first,second);
-        // }
-        // return dp[n];
-
-        int prev2 = 0;
-        int prev = 0;
-        int curr;
-        for(int i =2;i<=n;i++)
+        for(int i = 2;i<=n;i++)
         {
-            int first = cost[i-1]+prev;
-            int second = cost[i-2]+prev2;
-            curr = min(first,second);
-            prev2 = prev;
-            prev = curr;
-
+            int oneStep = cost[i-1] + dp[i-1];
+            int twoStep  = cost[i-2] + dp[i-2];
+            dp[i]= min(oneStep,twoStep);
         }
-        return prev;
-        
-
+        return dp[n];
     }
 };
