6c2086028d0a38c71504dc94980fa23bb88ec40b
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-04-13 16:21:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.8 MB (21.59%)

--- leetcode-solutions/0198-house-robber/solution.cpp
+++ leetcode-solutions/0198-house-robber/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
+    int solve(vector<int>&nums,int n, int index, vector<int>&dp)
+    {
+        if(index>=n)
+        {
+            return 0;
+        }
+        if(dp[index]!=-1)
+        {
+            return dp[index];
+        }
+        int take = nums[index] + solve(nums,n,index+2,dp);
+        int notTake = solve(nums,n,index+1,dp);
+        return dp[index] = max(take,notTake);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==1)
-        return nums[0];
-        int prev2 = 0;
-        int prev = nums[0];
-        int curr;
-        for(int i =2;i<=n;i++)
-        {
-            int steal = nums[i-1] + prev2;
-            int skip = prev;
-            curr = max(steal,skip);
-            prev2 = prev;
-            prev = curr;
-
-        }
-        return prev;
+        vector<int>dp(n+1,-1);
+        return solve(nums,n,0,dp);
     }
 };
