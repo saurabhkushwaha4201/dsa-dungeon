2dd90b724a31876e77517f901ee57062abf38681
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-04-17 04:54:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.5 MB (27.12%)

--- leetcode-solutions/0062-unique-paths/solution.cpp
+++ leetcode-solutions/0062-unique-paths/solution.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
+    int solve(int i, int j, int row, int col,vector<vector<int>>&dp)
+    {
+        if(i==row-1 && j==col-1)
+        {
+            return 1;
+        }
+        if(i>=row || j>= col)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        return dp[i][j] = solve(i+1,j,row,col,dp) + solve(i,j+1,row,col,dp);
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        for(int i =0;i<m;i++)
-        {
-            dp[i][0] = 1;
-        }
-        for(int i =0;i<n;i++)
-        {
-            dp[0][i] = 1;
-        }
-        for(int i = 1;i<m;i++)
-        {
-            for(int j = 1;j<n;j++)
-            {
-                dp[i][j] = dp[i-1][j]  + dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return solve(0,0,m,n,dp);
     }
 };
