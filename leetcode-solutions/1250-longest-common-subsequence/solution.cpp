c89503f84dfa0b979a2be171975bb5663fe0176b
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-05-22 04:14:25 +0000

    [LeetCode Sync] Runtime - 42 ms (25.67%), Memory - 27.5 MB (51.50%)

--- leetcode-solutions/1250-longest-common-subsequence/solution.cpp
+++ leetcode-solutions/1250-longest-common-subsequence/solution.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
+    int solve(string &text1, string &text2, int i, int j, vector<vector<int>>&dp) {
+        if (i < 0 || j <0) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if (text1[i] == text2[j]) {
+            return dp[i][j] = 1 + solve(text1, text2, i - 1, j - 1, dp);
+        }
+
+        return dp[i][j] = max(solve(text1, text2, i - 1, j, dp),solve(text1, text2, i, j - 1, dp));
+        
+    }
+
     int longestCommonSubsequence(string text1, string text2) {
         int len1 = text1.length();
         int len2 = text2.length();
-        vector<int>prev(len2+1,0),curr(len2+1,0);
-
-        for(int i = 1;i<=len1;i++)
-        {
-            for(int j = 1;j<=len2;j++)
-            {
-                if(text1[i-1]==text2[j-1])
-                {
-                    curr[j] = 1 + prev[j-1];
-                }
-                else
-                {
-                    curr[j] = max(prev[j],curr[j-1]);
-                }
-            }
-            prev = curr;
-        }
-        return prev[len2];
+        vector<vector<int>>dp(len1+1, vector<int>(len2+1, -1));
+        return solve(text1, text2, len1-1, len2-1, dp);
     }
 };
+
