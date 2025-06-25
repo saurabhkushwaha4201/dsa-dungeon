8dc2c0e38710801ed9b78ab86a3b4db4ea1b536d
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-06-25 16:16:32 +0000

    [LeetCode Sync] Runtime - 20 ms (10.14%), Memory - 19.7 MB (21.20%)

--- leetcode-solutions/0909-stone-game/solution.cpp
+++ leetcode-solutions/0909-stone-game/solution.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    int solve(int start , int end,vector<int>&piles,vector<vector<int>>&dp)
+    int solve(int start , int end, bool turn,vector<int>&piles,vector<vector<int>>&dp)
     {
-        if(start==end)
-        return piles[start];
+        if(start>end)
+        return 0;
 
-        if(dp[start][end]!=INT_MIN)
+        if(dp[start][end]!=-1)
         return dp[start][end];
-        
-        int firstChoice = piles[start] - solve(start+1,end,piles,dp);
-        int lastChoice = piles[end] - solve(start,end-1,piles,dp);
-        return dp[start][end] = max(firstChoice,lastChoice);
+        if(turn)
+        {
+            int choice1 = piles[start] + solve(start+1,end,false,piles,dp);
+            int choice2 = piles[end] + solve(start,end-1,false,piles,dp);
+            return dp[start][end] =  max(choice1,choice2);
+        }
+        else
+        {
+            int choice1 = -piles[start] + solve(start+1,end,true,piles,dp);
+            int choice2 = -piles[end] + solve(start,end-1,true,piles,dp);
+            return dp[start][end] = max(choice1,choice2);
+        }
         
     }
     bool stoneGame(vector<int>& piles) {
         
         int n = piles.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,INT_MIN));
-        return solve(0,n-1,piles,dp)>0;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(0,n-1,true,piles,dp)>0;
     }
 };
