7defba6dcf3db96c81e704ed069e6829baccc207
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-07-16 17:55:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.7 MB (12.01%)

---
+++ leetcode-solutions/1411-convert-binary-number-in-a-linked-list-to-integer/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        string temp = "";
+        while(head !=NULL)
+        {
+            temp.push_back(head->val + '0');
+            head = head->next;
+        }
+        return stoi(temp, nullptr, 2);
+
+
+        
+    
+    }
+};
