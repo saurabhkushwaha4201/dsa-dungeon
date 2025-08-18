0dc0810eb127fef8ca0d14f1f363d620e05081db
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-08-18 05:57:44 +0000

    [LeetCode Sync] Runtime - 10 ms (32.78%), Memory - 12.2 MB (82.57%)

--- leetcode-solutions/0237-delete-node-in-a-linked-list/solution.cpp
+++ leetcode-solutions/0237-delete-node-in-a-linked-list/solution.cpp
@@ -8,11 +8,10 @@
  */
 class Solution {
 public:
-    void deleteNode(ListNode* node) 
-    {
+    void deleteNode(ListNode* node) {
         ListNode* temp=node->next;
         node->val=temp->val;
         node->next=temp->next;
-       delete(temp);
+        delete(temp);
     }
 };
