af72b2aef6d92f07ff739b895f7ca0e2d11e0fda
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-10-03 18:41:01 +0000

    [LeetCode Sync] Runtime - 116 ms (9.93%), Memory - 39.4 MB (8.36%)

--- leetcode-solutions/0407-trapping-rain-water-ii/solution.cpp
+++ leetcode-solutions/0407-trapping-rain-water-ii/solution.cpp
@@ -1,60 +1,42 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,int>>PP;
-    vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    int trapRainWater(vector<vector<int>>& heightMap) 
-    {
-        int rows  = heightMap.size();
+    typedef pair<int,pair<int,int>>P;// {height,{i,j}}
+    vector<vector<int>> directions = {{-1,0}, {1,0}, {0,1}, {0,-1}};
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        int rows = heightMap.size();
         int cols = heightMap[0].size();
 
-        priority_queue<PP,vector<PP>,greater<>>boundaryCells;
+        priority_queue<P,vector<P>,greater<P>>cells;
         vector<vector<bool>>visited(rows,vector<bool>(cols,false));
 
-        // left most column and right most column (0,cols-1)
-
-        for (int r = 0; r < rows; ++r) {
-            for (int c : {0, cols - 1}) { //0 : left most boundary, cols-1 right most boundary
-                boundaryCells.push({heightMap[r][c], {r, c}});
-                visited[r][c] = true;
-            }
-        }
-
-        // top row and bottom row (0,rows-1)
-
-        for (int c = 0; c < cols; ++c) {
-            for (int r : {0, rows - 1}) { //0 : top most boundary, rows-1 bottom most boundary
-                boundaryCells.push({heightMap[r][c], {r, c}});
-                visited[r][c] = true;
-            }
-        }
-        
-        int trappedRain = 0;
-
-        while(!boundaryCells.empty())
-        {
-            auto [height, cell] = boundaryCells.top();
-            boundaryCells.pop();
-
-            int i = cell.first;
-            int j = cell.second;
-
-            // find neighbours
-            for(vector<int>&dir : directions)
-            {
-                int i_ = i + dir[0];
-                int j_ = j + dir[1];
-
-                if (i_ >= 0 && i_ < rows && j_ >= 0 && j_ < cols && !visited[i_][j_])
-                {
-                    trappedRain += max(0, height - heightMap[i_][j_]);
-                    
-                    boundaryCells.push({max(height, heightMap[i_][j_]), {i_, j_}});
-
-                    
-                    visited[i_][j_] = true;
+        // visit boundary cells first;
+        for(int i = 0;i<rows;i++){
+            for(int j = 0;j<cols;j++){
+                if(i==0 || i==rows-1 || j==0 || j==cols-1){
+                    cells.push({heightMap[i][j],{i,j}});
+                    visited[i][j] = true;
                 }
             }
         }
-        return trappedRain;
+        int waterFill = 0;
+        // now fill water to certain level with minimal height first
+        while(!cells.empty()){
+            int height = cells.top().first;
+            int i = cells.top().second.first;
+            int j = cells.top().second.second;
+            cells.pop();
+            for(auto dir:directions){
+                int new_i = i + dir[0];
+                int new_j = j+ dir[1];
+
+                // check out of boundary and not visited yet
+                if(new_i < rows && new_i >= 0 && new_j < cols && new_j >= 0 && !visited[new_i][new_j]){
+                    waterFill += max(height - heightMap[new_i][new_j],0);
+                    cells.push({max(height,heightMap[new_i][new_j]),{new_i,new_j}});
+                    visited[new_i][new_j] = true;
+                }
+            }
+        }
+        return waterFill;
     }
 };
