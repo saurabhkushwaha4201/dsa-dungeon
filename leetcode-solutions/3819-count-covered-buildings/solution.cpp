3eedd68deb5a8c33c857a57954519b9d34c0b11a
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-12-11 19:25:58 +0000

    [LeetCode Sync] Runtime - 26 ms (85.26%), Memory - 283.8 MB (82.11%)

--- leetcode-solutions/3819-count-covered-buildings/solution.cpp
+++ leetcode-solutions/3819-count-covered-buildings/solution.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        vector<int>rowMin(n+1,INT_MAX),rowMax(n+1,INT_MIN);
-        vector<int>colMin(n+1,INT_MAX),colMax(n+1,INT_MIN);
+        vector<int> maxRow(n + 1);
+        vector<int> minRow(n + 1, n + 1);
+        vector<int> maxCol(n + 1);
+        vector<int> minCol(n + 1, n + 1);
 
-        for(auto x:buildings)
-            {
-                int a = x[0];
-                int b = x[1];
-                rowMin[a] = min(rowMin[a],b);
-                rowMax[a] = max(rowMax[a],b);
-                colMin[b] = min(colMin[b],a);
-                colMax[b] = max(colMax[b],a);
+        for (auto& p : buildings) {
+            int x = p[0], y = p[1];
+            maxRow[y] = max(maxRow[y], x);
+            minRow[y] = min(minRow[y], x);
+            maxCol[x] = max(maxCol[x], y);
+            minCol[x] = min(minCol[x], y);
+        }
+
+        int res = 0;
+        for (auto& p : buildings) {
+            int x = p[0], y = p[1];
+            if (x > minRow[y] && x < maxRow[y] && y > minCol[x] &&
+                y < maxCol[x]) {
+                res++;
             }
-        int count = 0;
-        for(auto x:buildings)
-            {
-                int a = x[0];
-                int b = x[1];
-                if(rowMin[a]<b && rowMax[a]>b && colMin[b]<a && colMax[b]>a)
-                {
-                    count++;
-                }
-            }
-        return count;
+        }
+
+        return res;
     }
 };
